9b2386f2e340fe525a429c2e9bc7345d21871d3c
Adam Privette <aprivette@gmail.com>
2018-07-13 10:16:52 -0400

    Added settings to allow protecting an entire site or exclusively wp-login

--- README.md
+++ README.md
@@ -2,10 +2,12 @@
 
 A WordPress plugin designed to be an addon to the [Bedrock](https://roots.io/bedrock/) WordPress framework.
 
-It adds environment-specific basic authentication to WordPress sites. For example, if you were to set up a staging site and only wanted to protect only this setup while not affecting version control, then this package could be of use.
+It adds environment-specific basic authentication to WordPress sites. There are a few use cases for this.  For example, if you were to set up a staging site and only wanted to protect only this setup while not affecting version control, then this package could be of use.  Alternatively, you can exclusively protect wp-login.php to protect against brute force attacks.
 
 ## Setup
 
 1. Add the package to your bedrock installation with `composer require aprivette/bedrock-auth`.
-2. Add the `BASIC_AUTH_USER` and `BASIC_AUTH_PASS` variables to your .env file.
-3. Navigate to the site and it should require you to login.
+2. Activate the plugin in WordPress.
+3. Add the `BASIC_AUTH_USER` and `BASIC_AUTH_PASS` variables to your .env file.
+4. Choose an authentication level by setting the `BASIC_AUTH_LEVEL` variable to either site or login.  Choosing site will require auth for the entire site while login will exclusively protect wp-login.php.
+5. Navigate to the site and it should require you to login.
--- bedrock-auth.php
+++ bedrock-auth.php
@@ -30,19 +30,22 @@ class BasicAuth
     public function initAuth()
     {
         if (file_exists($this->root_dir . '/.env')) {
+            $this->dotenv->load();
+            $this->dotenv->required(['BASIC_AUTH_USER', 'BASIC_AUTH_PASS']);
+
             if (env('BASIC_AUTH_USER') && env('BASIC_AUTH_PASS')) {
-                $this->dotenv->load();
-                $this->dotenv->required(['BASIC_AUTH_USER', 'BASIC_AUTH_PASS']);
-                $this->requireAuth(env('BASIC_AUTH_USER'), env('BASIC_AUTH_PASS'));
+                if (strcasecmp(env('BASIC_AUTH_LEVEL'), 'site') === 0) {
+                    $this->requireAuth(env('BASIC_AUTH_USER'), env('BASIC_AUTH_PASS'));
+                } elseif (strcasecmp(env('BASIC_AUTH_LEVEL'), 'login') === 0 && substr($_SERVER['REQUEST_URI'], 0, 16) == '/wp/wp-login.php') {
+                    $this->requireAuth(env('BASIC_AUTH_USER'), env('BASIC_AUTH_PASS'));
+                }
             }
         }
     }
 
     // Adapted from https://gist.github.com/rchrd2/c94eb4701da57ce9a0ad4d2b00794131
     private function requireAuth($user, $pass) {
-        add_action('send_headers', function() {
-            header('Cache-Control: no-cache, must-revalidate, max-age=0');
-        });
+        header('Cache-Control: no-cache, must-revalidate, max-age=0');
 
         $has_supplied_credentials = !(empty($_SERVER['PHP_AUTH_USER']) && empty($_SERVER['PHP_AUTH_PW']));
 
@@ -53,15 +56,13 @@ class BasicAuth
         );
 
         if ($is_not_authenticated) {
-            add_action('send_headers', function() { 
-                header('HTTP/1.1 401 Authorization Required');
-                header('WWW-Authenticate: Basic realm="Access denied"');
-                wp_die(
-                    'Access denied.',
-                    'Authorization Required',
-                    array('response' => 401)
-                );
-            });
+            header('HTTP/1.1 401 Authorization Required');
+            header('WWW-Authenticate: Basic realm="Access denied"');
+            wp_die(
+                'Access denied.',
+                'Authorization Required',
+                array('response' => 401)
+            );
         }
     }
 }
